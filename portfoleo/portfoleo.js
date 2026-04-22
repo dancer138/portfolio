970b4cfce1a527b50a5775346e33b82415312388
dancer138 <lenovotab2020new@gmail.com>
2026-04-22 18:55:49 +0300

    dd
    dd

---
+++ portfoleo/portfoleo.js
@@ -0,0 +1,12 @@
+// const video = document.getElementById('myVideo');
+// const btn = document.getElementById('campaign__play-button');
+// // const poster = document.getElementById('CR7');
+// video.requestFullscreen();
+
+// btn.addEventListener('click', function() {
+//     video.play(); // Запускаем воспроизведение
+    
+//     // Опционально: скрываем кнопку и картинку после нажатия
+//     btn.style.display = 'none';
+//     poster.style.display = 'none';
+// });
