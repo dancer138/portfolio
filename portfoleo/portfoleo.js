2d87a11afa8f0117545baa5390a822b97ee6e22a
dancer138 <lenovotab2020new@gmail.com>
2026-05-18 18:55:20 +0300

    STATISTIC-PHOTO
    FFF

---
+++ portfoleo/portfoleo.js
@@ -0,0 +1,8 @@
+// let age =  "Вы принимаете файлы  cookie!";
+// alert(age);
+// let password;
+// do {
+//     password = prompt("Введите пароль:");
+// } while (password !== "CR7");
+// alert ("подтверждено");
+// console.log("пароль принят!");
