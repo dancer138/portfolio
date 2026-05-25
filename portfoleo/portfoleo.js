66d66289bc52bea122adb750d07d6d2493c96856
dancer138 <lenovotab2020new@gmail.com>
2026-05-25 18:25:36 +0300

    ffff
    fff

--- portfoleo/portfoleo.js
+++ portfoleo/portfoleo.js
@@ -6,3 +6,14 @@
 // } while (password !== "CR7");
 // alert ("подтверждено");
 // console.log("пароль принят!");
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("newsletter-form");
+    const emailInput = document.getElementById("email-input");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log("Email пользователя:", emailInput.value);
+    });
+  });
